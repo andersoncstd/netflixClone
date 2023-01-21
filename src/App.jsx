b360988d90f08a3f68fca40276b8ba733e33fcde
José Anderson Custódio <anderson.k1006@gmail.com>
2023-01-21 12:48:12 -0300

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import NavBar  from './components/NavBar/NavBar'
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+    </>
+    );
+  }
+
+export default App
