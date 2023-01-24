6913a577eac4fa6c92bb4b00524dd795121b8a75
José Anderson Custódio <anderson.k1006@gmail.com>
2023-01-23 21:11:46 -0300

    unsaved file

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -13,7 +13,6 @@ export default function MovieCard(props) {
             </ul>
                 <img className="poster" src={poster} alt={`Assistir ${name}`} />
             </div>
-
         </div>
         </>
     )
